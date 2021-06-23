bbe79ea5f63f7638e2e16609439805027a91b5d2
Stanislau Petrusiou <stanislau.petrusiou@pan-net.eu>
2021-06-23 13:09:09 +0200

    Fixed comparator to strings.EqualFold

--- powerdns/client.go
+++ powerdns/client.go
@@ -522,7 +522,7 @@ func (client *Client) ListRecordsInRRSet(zone string, name string, tpe string) (
 
 	records := make([]Record, 0, 10)
 	for _, r := range allRecords {
-		if r.Name == strings.ToLower(name) && r.Type == tpe {
+		if strings.EqualFold(r.Name, name) && r.Type == tpe {
 			records = append(records, r)
 		}
 	}
@@ -547,7 +547,7 @@ func (client *Client) RecordExists(zone string, name string, tpe string) (bool, 
 	}
 
 	for _, record := range allRecords {
-		if record.Name == strings.ToLower(name) && record.Type == tpe {
+		if strings.EqualFold(record.Name, name) && record.Type == tpe {
 			return true, nil
 		}
 	}
